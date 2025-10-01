dbb32c20374c002e7bf3ca17430a65d26ecd27dc
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-10-01 15:25:35 +0200

    teleport

---
+++ PIO_Lectures/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class TeleportController : MonoBehaviour
+{
+    public InputActionProperty teleportActivate;
+    public XRRayInteractor rightRay;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rightRay.gameObject.SetActive(false);
+
+        teleportActivate.action.Enable();
+        teleportActivate.action.performed += ActionPerformed;
+        teleportActivate.action.canceled += ActionCanceled;
+    }
+
+    private void ActionPerformed(InputAction.CallbackContext obj)
+    {
+        rightRay.gameObject.SetActive(true);
+    }
+
+    private void ActionCanceled(InputAction.CallbackContext obj)
+    {
+        rightRay.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
