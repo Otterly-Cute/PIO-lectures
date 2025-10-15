e2f7be3d16fe31beb6c3b7a6933b2d8d5a8da026
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2025-10-15 12:44:41 +0200

    script

--- PIO_Lectures/Assets/Scripts/CatActivate.cs
+++ PIO_Lectures/Assets/Scripts/CatActivate.cs
@@ -19,6 +19,29 @@ public class CatActivate : MonoBehaviour
         
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (this.gameObject == britishCat)
+            {
+                britishCat.SetActive(true);
+                ActivateGlow(purple);
+            }
+            else if (this.gameObject == angoraCat)
+            {
+                angoraCat.SetActive(true);
+                ActivateGlow(blue);
+            }
+            else if (this.gameObject == ragdollCat)
+            {
+                ragdollCat.SetActive(true);
+                ActivateGlow(green);
+            }
+        }
+    }
+
+
     public void ActivateGlow (Material color)
     {
         color.EnableKeyword("_EMISSION");
